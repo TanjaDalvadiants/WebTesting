d534eaba9d4c47241cf68b7370a49a90eb557045
tatiana dalvadiants <eparis@atlassian.com>
2023-05-03 22:07:59 +0400

    negativ path added

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -12,13 +12,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebTest {
-     WebDriver driver;
+    WebDriver driver;
 
     @BeforeAll
     static void setupAll() {
+
         WebDriverManager.chromedriver().setup();
     }
-
 
 
     @BeforeEach
@@ -37,18 +37,107 @@ public class WebTest {
     }
 
     @Test
-    void shouldTestSelenium() {
+    void shouldTestHappyPathSelenium() {
 
         driver.get("http://localhost:9999/");
         driver.findElements(By.tagName("input")).get(0).sendKeys("Татьяна");
         driver.findElements(By.tagName("input")).get(1).sendKeys("+79042424072");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
+
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
         assertEquals(expected, actual);
+    }
+    @Test
+    void shouldTestLatinNameSelenium() {
 
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Tatyana");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("+79042424072");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
 
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.className("input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldTestNameWithNumbersSelenium() {
+
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Татьяна1");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("+79042424072");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.className("input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldTestEmptyNameSelenium() {
+
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.tagName("input")).get(0).sendKeys(" ");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("+79042424072");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.className("input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldTestLongPhoneSelenium() {
+
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Татьяна");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("834329423874982749287");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.xpath("/html/body/div/div/form/div[2]/span/span/span[3]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldTestShortPhoneSelenium() {
+
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Татьяна");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("8343");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.xpath("/html/body/div/div/form/div[2]/span/span/span[3]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldTestEmptyPhoneSelenium() {
+
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Татьяна");
+        driver.findElements(By.tagName("input")).get(1).sendKeys(" ");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.xpath("/html/body/div/div/form/div[2]/span/span/span[3]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldTestAgreementNotCheckSelenium() {
+
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Татьяна");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("+79042424072");
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='agreement']")).getText().trim();
+        assertEquals(expected, actual);
     }
 
 
